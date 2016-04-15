1b327a2d778b87be233287363cf09130f984ab41
David Lopez <Dave42493@gmail.com>
2016-04-15 01:16:40 -0400

    add sub,mult,div function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 var add = function(number1, number2) {
   return number1 + number2;
 };
-
+var subtract =function(number1,number2){
+  return number1 - number2;
+};
+var multiply =function(number1,number2){
+  return number1*number2;
+};
+var divide=function(number1,number2){
+  return number1/number2;
+};
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+var addresult = add(number1, number2);
+var subresult = subtract(number1, number2);
+var multresult = multiply(number1, number2);
+var divresult = divide(number1, number2);
+alert("add:"+addresult+", subtract:"+subresult+", multiply:"+multresult+", divide:"+divresult);
